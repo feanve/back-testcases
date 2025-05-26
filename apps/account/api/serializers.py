fd45050719906964f964d08f2537b7dd584f5df3
Felipe Andres Velez <felipeandresvelez@gmail.com>
2025-05-25 22:01:59 -0500

    Se agrega rol al modelo de user

--- apps/account/api/serializers.py
+++ apps/account/api/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'is_active', 'created_at', 'updated_at')
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'is_active', 'rol', 'created_at', 'updated_at')
         read_only_fields = ('id', 'created_at', 'updated_at')
 
 class RegisterSerializer(serializers.ModelSerializer):
